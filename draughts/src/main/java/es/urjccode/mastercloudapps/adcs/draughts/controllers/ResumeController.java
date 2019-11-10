6fe445f6f84479c3f1e413f2fb4b8650c84b48cb
AdrianPedriza <w1nchester>
2019-11-10 12:27:21 +0100

    delete Session abstraction

--- draughts/src/main/java/es/urjccode/mastercloudapps/adcs/draughts/controllers/ResumeController.java
+++ draughts/src/main/java/es/urjccode/mastercloudapps/adcs/draughts/controllers/ResumeController.java
@@ -1,12 +1,13 @@
 package es.urjccode.mastercloudapps.adcs.draughts.controllers;
 
-import es.urjccode.mastercloudapps.adcs.draughts.models.Session;
+import es.urjccode.mastercloudapps.adcs.draughts.models.Game;
+import es.urjccode.mastercloudapps.adcs.draughts.models.State;
 
 public class ResumeController extends Controller {
 
-	public ResumeController(Session session) {
-        super(session);
-    }
+	public ResumeController(Game game, State state) {
+		super(game, state);
+	}
     
     @Override
 	public void accept(ControllersVisitor controllersVisitor) {
@@ -14,11 +15,11 @@ public class ResumeController extends Controller {
 	}
 
 	public void next() {
-        this.session.next();
+        this.state.next();
 	}
 
 	public void reset() {
-        this.session.reset();
+        this.state.reset();
 	}
 
 }
